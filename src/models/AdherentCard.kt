724211b46c1b85ccdaae0587df933d20dcb479a3
Haydart <makowiecki.radoslaw@gmail.com>
2018-02-24 16:43:11 +0100

    Specify actions function for a plain adherent card

--- src/models/AdherentCard.kt
+++ src/models/AdherentCard.kt
@@ -1,14 +1,25 @@
 package models
 
+val defaultAdherentActionsFun: (Card, Player, Player) -> List<Action> = { triggeringCard, (handCards), enemyPlayer ->
+    val availableActions = mutableListOf<Action>()
+    val isInHand = triggeringCard in handCards
+
+    enemyPlayer.tableCards.forEach {
+        availableActions += Action.HitOne(triggeringCard, it, (triggeringCard as AdherentCard).attackStrength)
+    }
+
+    if(isInHand) availableActions += Action.DrawCard(triggeringCard)
+    availableActions
+}
+
 class AdherentCard(
         val maxHealthPoints: Int,
         var attackStrength: Int,
         var lastTurnPlaced: Int = -1,
         name: String,
         manaCost: Int
-) : Card(name, manaCost) {
+) : Card(name, manaCost, defaultAdherentActionsFun) {
 
     var currentHealthPoints: Int = maxHealthPoints
-
 
 }
